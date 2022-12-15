f4b54017a9bacb61dbf84aa16ef44630d98476a8
ChillYall <mariellehoneypchiong@gmail.com>
2022-12-15 14:05:03 +0800

    more fixes

--- scripts/juice-created.js
+++ scripts/juice-created.js
@@ -1,4 +1,4 @@
-const total = document.getElementById("totalJuiceCreated");
+const total = document.getElementById("totalJuice");
 
 const totalJuiceCreated = localStorage.getItem("totalJuice");
 localStorage.setItem("totalJuice", total);
